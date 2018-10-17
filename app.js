b221091075906b620246bdb009fd247db558e91e
Shubham Sethi <shubhamsethikcm@gmail.com>
2018-10-17 15:24:57 +0530

    xyz

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ var corsOptions = {
   optionsSuccessStatus: 200
 };
  app.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin');
-//   res.setHeader('Access-Control-Allow-Methods', 'POST');
+   res.setHeader('Access-Control-Allow-Origin');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 //   res.setHeader('Access-Control-Allow-Credentials', true);
    next();
